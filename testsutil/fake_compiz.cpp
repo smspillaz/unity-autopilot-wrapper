414acedfd2cf9155968266685b4737ae86d773f4
Sam Spilsbury <smspillaz@gmail.com>
2013-06-04 12:37:22 +0800

    Remove the complicated poll () sleep mechanism and just use pause ()

--- testsutil/fake_compiz.cpp
+++ testsutil/fake_compiz.cpp
@@ -8,55 +8,14 @@
  * See LICENSE.md for Copyright information.
  */
 
-#include <stdexcept>
 #include <iostream>
-
-#include <stdlib.h>
-
-#include <fcntl.h>
-#include <signal.h>
 #include <unistd.h>
-#include <sys/poll.h>
-#include <cstring>
-#include <errno.h>
 
-void signal (int sig)
+int main ()
 {
-    exit (0);
-}
-
-int main (int argc, char const *argv[])
-{
-    int p[2];
-    pipe2 (p, O_CLOEXEC);
-
     std::cout << "initLauncher-hud" << std::endl;
 
-    /* Repeat arguments and sleep */
-    for (int i = 0; i < argc; ++i)
-        std::cout << argv[i] << std::endl;
-
-    struct pollfd pfd;
-
-    pfd.events = POLLIN | POLLOUT | POLLHUP;
-    pfd.revents = 0;
-    pfd.fd = p[0];
-
-    while (1)
-    {
-        poll (&pfd, 1, -1);
-
-        if (pfd.revents)
-        {
-            char buf[1024];
-            ssize_t amount = read (pfd.fd, static_cast <void *> (buf), 1023);
-            if (amount == -1)
-                throw std::runtime_error (strerror (errno));
-        }
-    }
-
-    close (p[0]);
-    close (p[1]);
+    pause ();
 
     return 0;
 }
--- testsutil/fake_server.cpp
+++ testsutil/fake_server.cpp
@@ -8,17 +8,10 @@
  *
  */
 
-#include <stdexcept>
-#include <iostream>
-
 #include <stdlib.h>
 
-#include <fcntl.h>
 #include <signal.h>
 #include <unistd.h>
-#include <sys/poll.h>
-#include <cstring>
-#include <errno.h>
 
 void signal (int sig)
 {
@@ -32,32 +25,7 @@ int main (int argc, char const *argv[])
     if (handler == SIG_IGN)
         kill (getppid (), SIGUSR1);
 
-    int p[2];
-    pipe2 (p, O_CLOEXEC);
+    pause ();
 
-    /* Repeat arguments and sleep */
-    for (int i = 0; i < argc; ++i)
-        std::cout << argv[i] << std::endl;
-
-    struct pollfd pfd;
-
-    pfd.events = POLLIN | POLLOUT | POLLHUP;
-    pfd.revents = 0;
-    pfd.fd = p[0];
-
-    while (1)
-    {
-        poll (&pfd, 1, -1);
-
-        if (pfd.revents)
-        {
-            char buf[1024];
-            ssize_t amount = read (pfd.fd, static_cast <void *> (buf), 1023);
-            if (amount == -1)
-                throw std::runtime_error (strerror (errno));
-        }
-    }
-
-    close (p[0]);
-    close (p[1]);
+    return 0;
 }
